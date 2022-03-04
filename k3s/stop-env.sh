c7f4ceedf3478290c567e25d3e4961119a13025a
pipeline role <codepipeline@hmpotr.com>
2022-03-04 11:32:30 -0300

    fixed stop-env to delete all cronjobs before the pods

--- k3s/stop-env.sh
+++ k3s/stop-env.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-kubectl delete deployments --all && kubectl delete pods --all && kubectl delete service --all && kubectl delete jobs.batch --all && kubectl delete cronjobs.batch --all
+kubectl delete cronjobs.batch --all && \
+kubectl delete jobs.batch --all  && \
+kubectl delete deployments --all && \
+kubectl delete pods --all && \
+kubectl delete service --all
