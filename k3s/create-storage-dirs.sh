9723d1fb9f725c2229a230bf26f9739e2b0ad71a
pipeline role <codepipeline@hmpotr.com>
2022-03-04 11:10:43 -0300

    fixed create-storage-dirs script to find the tar file in the right place

--- k3s/create-storage-dirs.sh
+++ k3s/create-storage-dirs.sh
@@ -6,7 +6,7 @@ cd $DIR
 if [[ ! -d "${PV_STORAGE_BASE}" ]]; then 
 mkdir "${PV_STORAGE_BASE}"
 cd "${PV_STORAGE_BASE}"
-tar xvfz ../sample-storage.tar.gz 
+tar xvfz ${DIR}/../sample-storage.tar.gz 
 sudo chmod -R 777 *
 
 fi
