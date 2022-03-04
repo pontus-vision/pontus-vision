1aa4e307a382396e502b5209b6a04dc85798abbd
pipeline role <codepipeline@hmpotr.com>
2022-03-04 11:01:39 -0300

    wip

--- k3s/create-storage-dirs.sh
+++ k3s/create-storage-dirs.sh
@@ -6,26 +6,7 @@ cd $DIR
 if [[ ! -d "${PV_STORAGE_BASE}" ]]; then 
 mkdir "${PV_STORAGE_BASE}"
 cd "${PV_STORAGE_BASE}"
-mkdir -p extract/email \
-    extract/CRM \
-    extract/ERP \
-    extract/microsoft/data-breaches \
-    extract/microsoft/dsar \
-    extract/microsoft/fontes-de-dados \
-    extract/microsoft/legal-actions \
-    extract/microsoft/mapeamentos \
-    extract/google/meetings \
-    extract/google/policies \
-    extract/google/privacy-docs \
-    extract/google/privacy-notice \
-    extract/google/risk \
-    extract/google/risk-mitigations \
-    extract/google/treinamentos \
-  db \
-  grafana \
-  keycloak \
-  timescaledb
-
+tar xvfz ../sample-storage.tar.gz 
 sudo chmod -R 777 *
 
 fi
