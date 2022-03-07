130a0126c259d23b7f1c736b97e4fd338fac8568
pipeline role <codepipeline@hmpotr.com>
2022-03-07 10:36:49 -0300

    simplified start-graph-gdpr/lgpd logic

--- k3s/start-env-lgpd.sh
+++ k3s/start-env-lgpd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  -x 
+#!/bin/bash
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR
--- k3s/start-graph-gdpr.sh
+++ k3s/start-graph-gdpr.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $DIR
-./create-env-secrets.sh
-if [[ -z ${PV_HOSTNAME} ]]; then
-  export PV_HOSTNAME=$(hostname)
-fi
-if [[ -z ${PV_STORAGE_BASE} ]]; then
-  export PV_STORAGE_BASE=${DIR}/storage
-fi
-if [[ ! -d ${PV_STORAGE_BASE} ]]; then
- ./create-storage-dirs.sh
-fi
-
-envsubst ./helm/values-gdpr.yaml > ./helm/values-gdpr-resolved.yaml
-
-helm template -s templates/graphdb.yaml -f ./helm/values-gdpr-resolved.yaml pv ./helm/pv | k3s kubectl apply -f -
+start-graph-lgpd.sh
